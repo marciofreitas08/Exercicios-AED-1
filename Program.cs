b828eff3c782ac542aa8bb3f1f1a46b3df62c8a3
marciofreitas08 <50387971+marciofreitas08@users.noreply.github.com>
2019-12-11 22:12:34 -0300

    Ex notas dos alunos

--- Program.cs
+++ Program.cs
@@ -1,59 +1,35 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Globalization;
 
-namespace exercicio_aula_classe_trem_vagao
+namespace exer_classe_nota_dos_alunos
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Alunos aluno = new Alunos();
 
-            /* - Crie um programa em C# para representar um trem de carga. Cada trem possui apenas 3 vagões,
-               a informação da sua velocidade e a carga máxima suportada pela locomotiva. Cada vagão possui
-               uma descrição do material presente e a quantidade carregada (em toneladas). 
+            Console.Write("Nome do aluno: ");
+            aluno.Nome = Console.ReadLine();
 
-               - Devem ser criadas classes para a representação do trem e dos vagões.
-               - Crie as classes e pelo menos 2 atributos de tipos diferentes em cada classe. 
-               - Crie 1 método construtor com parâmetros e um sem parâmetros para cada classe. 
-               - Crie pelo menos 1 par de métodos get/set para cada classe.
-               - Crie o método CargaRestante que retorna a quantidade de carga que o trem ainda consegue suportar
-               com base na carga carregada em seus vagões. 
-               - Crie o método bool TransferirCargaPara(Trem destino) que permite transferir a carga de um trem para
-               outro mantendo a quantidade carregada em cada vagão. Só deve ser possível realizar a transferência se
-               o trem de destino estiver vazio e tiver capacidade para receber a carga. O método deve indicar como
-               retorno se a transferência foi realizada com sucesso ou não.
-               - Crie um método main para fazer uso da classe criada com pelo menos 2 objetos Trem e uso de 2 dos
-               métodos  (diferentes de get/set) criados.
-               */
-            Vagao v1 = new Vagao( "V1","carvao", 80.0);
-            Vagao v2 = new Vagao("V2", "minerio", 40.0);
-            Vagao v3 = new Vagao("V3","coque", 40.0);
+            Console.WriteLine("Digite as três notas do aluno:");
+            aluno.Nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            aluno.Nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            aluno.Nota3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            Trem num1 = new Trem ("Trem 1",30, 200, v1, v2, v3);
+            Console.WriteLine("NOTA FINAL = " + aluno.NotaFinal().ToString("F2", CultureInfo.InvariantCulture));
 
-            Vagao k1 = new Vagao("K1","carvao", 10.0);
-            Vagao k2 = new Vagao("K2","minerio", 10.0);
-            Vagao k3 = new Vagao("K3","coque", 40.0);
-
-            Trem num2 = new Trem("Trem 2",30, 20, k1, k2, k3);
-
-            v1.TransferirCarga(k1, 20.0);
-
-            Console.WriteLine();
-
-            Console.WriteLine( num1);
-
-            Console.WriteLine();
-                                 
-            Console.WriteLine( num2);
-
-           
-                 
-
-
+            if (aluno.Aprovado())
+            {
+                Console.WriteLine("APROVADO");
+            }
+            else
+            {
+                Console.WriteLine("REPROVADO");
+                Console.WriteLine("FALTARAM "
+                + aluno.NotaRestante().ToString("F2", CultureInfo.InvariantCulture)
+                + " PONTOS");
+            }
         }
-    }
+    }  
 }
