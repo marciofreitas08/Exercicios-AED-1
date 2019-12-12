83af60e33b376204ebf04abccfb1b762a0c55bca
marciofreitas08 <50387971+marciofreitas08@users.noreply.github.com>
2019-12-11 22:36:18 -0300

    Add files via upload

---
+++ Aluno.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaAlunos1
+{
+    class Aluno
+    {
+        private string _nome;
+        public string nome
+        {
+            get
+            {
+                return this._nome;
+            }
+            set
+            {
+                this._nome = value;
+            }
+
+        }
+
+        private double _nota1;
+        public double nota1
+        {
+            get
+            {
+                return this._nota1;
+
+            }
+            set
+            {
+                this._nota1 = value;
+            }
+        }
+
+        private double _nota2;
+        public double nota2
+        {
+            get
+            {
+                return this._nota2;
+
+            }
+            set
+            {
+                this._nota2 = value;
+            }
+        }
+
+        private double _nota3;
+        public double nota3
+        {
+            get
+            {
+                return this._nota3;
+
+            }
+            set
+            {
+                this._nota3 = value;
+            }
+        }
+
+        private double _nota4;
+        public double nota4
+        {
+            get
+            {
+                return this._nota4;
+
+            }
+            set
+            {
+                this._nota4 = value;
+            }
+        }
+
+        public double media()
+        {
+            return (this._nota1 + this._nota2 + this._nota3 + this._nota4) / 4;
+        }
+
+        public void exibeMedia()
+        {
+            Console.Clear();
+            Console.WriteLine(" Nome:" + this._nome);
+            Console.WriteLine(" Nota1:" + this._nota1);
+            Console.WriteLine(" Nota2:" + this._nota2);
+            Console.WriteLine(" Nota3:" + this._nota3);
+            Console.WriteLine(" Nota4:" + this._nota3);
+            Console.WriteLine(" Media das notas" + this.media());
+        }
+
+
+
+
+
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,28 +4,37 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Metodos_estaticos_calculadora
+namespace MediaAlunos1
 {
     class Program
     {
+        //calcular a media de um aluno informar 4 notas
         static void Main(string[] args)
         {
-            /* Crie uma classe chamada CalculadoraInt que possua como atributos 2 operandos
-             * e possua as seguintes operações (métodos):
+            Aluno obj = new Aluno();
+            String entrada = " ";
+            Console.WriteLine(" Calcula a media de um aluno:");
+            Console.Write(" Aluno:");
+            entrada = Console.ReadLine();
+            obj.nome = entrada;
 
-               Métodos não estático para as operações Soma, Subtração, Divisão,
-               Multiplicação para a realização das operações básicas com os operandos da
-               calculadora.
-               Método estático chamado Soma que recebe 2 valores inteiros como parâmetros
-               e retorna a soma desses dois valores. Crie também o teste de utilização do
-               método dentro do método main.
-               Método não estático chamado Maior que retorna o maior dos dois operandos.
-               Crie também o teste de utilização do método dentro do método main.
-               */
+            Console.Write(" Nota1:");
+            entrada = Console.ReadLine();
+            obj.nota1 = Convert.ToDouble(entrada);
 
-            Calculadora numero = new Calculadora(2,3);
-            numero.Soma();
+            Console.Write(" Nota2:");
+            entrada = Console.ReadLine();
+            obj.nota2 = Convert.ToDouble(entrada);
 
+            Console.Write(" Nota3:");
+            entrada = Console.ReadLine();
+            obj.nota3 = Convert.ToDouble(entrada);
+
+            Console.Write(" Nota4:");
+            entrada = Console.ReadLine();
+            obj.nota4 = Convert.ToDouble(entrada);
+
+            obj.exibeMedia();
         }
     }
 }
