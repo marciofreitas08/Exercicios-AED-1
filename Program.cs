3b032a7836fbc0b86a8ef2c1225757c8e5596beb
marciofreitas08 <50387971+marciofreitas08@users.noreply.github.com>
2019-12-11 22:25:00 -0300

    Ex Par ou impar

--- Program.cs
+++ Program.cs
@@ -4,52 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace exercicios_ESO_divisivel_por_2_3_5_7
+namespace exercicios_do_ssatela_soft
 {
     class Program
     {
-       /* Fazer um programa que leia vários números(um por vez) e imprima 0 (zero) se o número
-          não for múltiplo de 2,3, 5 nem 7. Imprimir 1(um) se o número for múltiplo de 2 ou 3 
-          e 2(dois) se o número for múltiplo de 5 e 7. Sair do programa quando for digitado
-          o valor -1. (Não é necessária nenhuma consistência ou crítica dos dados).
-          */
-
         static void Main(string[] args)
         {
-            while (true)
+            Console.WriteLine(" num1 ");
+            int num1 = int.Parse(Console.ReadLine());
+
+            if (num1 % 2 == 0)
             {
-                Console.WriteLine(" Digite um numero qualquer:");
-                int a = int.Parse(Console.ReadLine());
-
-                if (a % 2 != 0 && a % 3 != 0 && a % 5 != 0 && a % 7 != 0)
-                {
-                    Console.WriteLine("0");
-
-                }
-
-                else if (a % 2 == 0 || a % 3 == 0)
-                {
-                    Console.WriteLine("1");
-                }
-
-                else if (a % 5 == 0 || a % 7 == 0)
-                {
-                    Console.WriteLine("2");
-
-                }
-
-                Console.WriteLine(" Aperte -1 para sair");
-                int num = int.Parse(Console.ReadLine());
-
-                if (num == -1)
-                {
-                    break;
-                }
-
-
+                Console.WriteLine(" numero par:");
 
             }
-            
+
+            else
+            {
+                Console.WriteLine(" numero impar:");
+            }
+
         }
     }
 }
