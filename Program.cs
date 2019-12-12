5df1984bdeac2c75a519c2e34e4d62bb0253f77c
marciofreitas08 <50387971+marciofreitas08@users.noreply.github.com>
2019-12-11 22:08:48 -0300

    ex Udemy retangulo

--- Program.cs
+++ Program.cs
@@ -1,48 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Globalization;
 
-namespace Ex_udemy_classes_produto
+
+namespace exer_classes_retangulo
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Produto p = new Produto();
+            Retangulo ret = new Retangulo();
 
-            Console.WriteLine(" Entre os dados do produto:");
-            Console.Write(" Nome:");
-            p.Nome = Console.ReadLine();
-            Console.Write("Preço:");
-            p.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            Console.Write("Quantidade no estoque: ");
-            p.Quantidade = int.Parse(Console.ReadLine());
+            Console.WriteLine("Entre a largura e altura do retângulo: ");
+            ret.Largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            ret.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            Console.WriteLine();
-
-            Console.WriteLine("Dados do produto:" + p);
-
-            Console.WriteLine();
-            Console.WriteLine(" Digite o numero de produtos a ser adicionado ao estoque:");
-            int qte = int.Parse(Console.ReadLine());
-            p.AdicionarProdutos(qte);
-            Console.WriteLine(" Dados atualizados:" + p);
-
-            Console.WriteLine();
-            Console.WriteLine(" Digite o numero de produtos a ser removido ao estoque:");
-            qte = int.Parse(Console.ReadLine());
-            p.RemoverProdutos(qte);
-            Console.WriteLine(" Dados atualizados:" + p);
-
-
-
-
-
-
-
+            Console.WriteLine("AREA = " + ret.Area().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("PERIMETRO = " + ret.Perimetro().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("DIAGONAL = " + ret.Diagonal().ToString("F2", CultureInfo.InvariantCulture));
 
 
         }
---
+++ Retangulo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exer_classes_retangulo
+{
+    class Retangulo
+    {
+        public double Largura;
+        public double Altura;
+
+        public double Area()
+        {
+            return Altura * Largura;
+        }
+
+        public double Perimetro()
+        {
+            return (Altura + Largura) * 2;
+        }
+        public double Diagonal()
+        {
+            return Math.Sqrt(Largura * Largura + Altura * Altura);
+        }
+
+
+
+
+
+
+    }
+}
