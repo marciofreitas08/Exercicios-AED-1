86c08cf9d78bb401d23bed9106a6be02edffb8fb
marciofreitas08 <50387971+marciofreitas08@users.noreply.github.com>
2019-12-11 22:03:55 -0300

    Exercício Udemy Produto

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Ex_udemy_classes_produto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p = new Produto();
+
+            Console.WriteLine(" Entre os dados do produto:");
+            Console.Write(" Nome:");
+            p.Nome = Console.ReadLine();
+            Console.Write("Preço:");
+            p.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantidade no estoque: ");
+            p.Quantidade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            Console.WriteLine("Dados do produto:" + p);
+
+            Console.WriteLine();
+            Console.WriteLine(" Digite o numero de produtos a ser adicionado ao estoque:");
+            int qte = int.Parse(Console.ReadLine());
+            p.AdicionarProdutos(qte);
+            Console.WriteLine(" Dados atualizados:" + p);
+
+            Console.WriteLine();
+            Console.WriteLine(" Digite o numero de produtos a ser removido ao estoque:");
+            qte = int.Parse(Console.ReadLine());
+            p.RemoverProdutos(qte);
+            Console.WriteLine(" Dados atualizados:" + p);
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
