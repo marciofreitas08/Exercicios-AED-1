443fac41a4358dc533d1eda4bc7c7585dff070a9
marciofreitas08 <50387971+marciofreitas08@users.noreply.github.com>
2019-12-11 22:24:06 -0300

    Ex divisivel por 2,3,5,7

--- Program.cs
+++ Program.cs
@@ -1,20 +1,55 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace Marcio1
+namespace exercicios_ESO_divisivel_por_2_3_5_7
 {
     class Program
     {
+       /* Fazer um programa que leia vários números(um por vez) e imprima 0 (zero) se o número
+          não for múltiplo de 2,3, 5 nem 7. Imprimir 1(um) se o número for múltiplo de 2 ou 3 
+          e 2(dois) se o número for múltiplo de 5 e 7. Sair do programa quando for digitado
+          o valor -1. (Não é necessária nenhuma consistência ou crítica dos dados).
+          */
+
         static void Main(string[] args)
         {
-            int numero = 0;
-            Console.WriteLine("Entre com numero:");
-            numero = int.Parse(Console.ReadLine());
-            while (numero > 0)
+            while (true)
             {
-                Console.WriteLine(" Vou subtrair 2 do numero escolhido");
-                numero = numero - 2;
-                Console.WriteLine(" O numero agora é: {0}",numero);
+                Console.WriteLine(" Digite um numero qualquer:");
+                int a = int.Parse(Console.ReadLine());
+
+                if (a % 2 != 0 && a % 3 != 0 && a % 5 != 0 && a % 7 != 0)
+                {
+                    Console.WriteLine("0");
+
+                }
+
+                else if (a % 2 == 0 || a % 3 == 0)
+                {
+                    Console.WriteLine("1");
+                }
+
+                else if (a % 5 == 0 || a % 7 == 0)
+                {
+                    Console.WriteLine("2");
+
+                }
+
+                Console.WriteLine(" Aperte -1 para sair");
+                int num = int.Parse(Console.ReadLine());
+
+                if (num == -1)
+                {
+                    break;
+                }
+
+
+
             }
+            
         }
     }
 }
