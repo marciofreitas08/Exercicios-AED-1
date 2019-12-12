046e2c827375a43efe39ffd1c283bcd8727cab27
marciofreitas08 <50387971+marciofreitas08@users.noreply.github.com>
2019-12-11 22:31:48 -0300

    Ex calculadora

---
+++ Calculadora.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metodos_estaticos_calculadora
+{
+    class Calculadora
+    {
+        private int num1, num2;
+
+        public int getNum1()
+        {
+            return num1;
+        }
+        public int getNum2()
+        {
+            return num2;
+        }
+
+        public void setNum1(int n)
+        {
+            num1 = n;
+        }
+
+        public void setAno(int n2)
+        {
+            num2 = n2;
+        }
+
+        public Calculadora(int n,int n2)
+        {
+            num1 = n;
+
+            num2 = n2;
+        }
+
+        public int Soma()
+        {
+             return num1 + num2;
+        }
+
+        public int Subtracao()
+        {
+            if(num1 < num2)
+            {
+                 return num2 - num1;
+            }
+            else
+            {
+                return num1 - num2;
+
+            }
+           
+           
+        }
+        public int Divisao()
+        {
+            return num1 / num2;
+        }
+
+        public int Multiplicacao()
+        {
+            return num1 * num2;
+        }
+
+       
+
+
+
+
+
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,15 +4,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Herança1
+namespace Metodos_estaticos_calculadora
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Fisica fulano = new Fisica(1291728, 'M', "Estudante", "Jão", "02/03/2002", "São Paulo", "jaosp@gmail.com");
+            /* Crie uma classe chamada CalculadoraInt que possua como atributos 2 operandos
+             * e possua as seguintes operações (métodos):
 
-            Console.WriteLine(fulano);
+               Métodos não estático para as operações Soma, Subtração, Divisão,
+               Multiplicação para a realização das operações básicas com os operandos da
+               calculadora.
+               Método estático chamado Soma que recebe 2 valores inteiros como parâmetros
+               e retorna a soma desses dois valores. Crie também o teste de utilização do
+               método dentro do método main.
+               Método não estático chamado Maior que retorna o maior dos dois operandos.
+               Crie também o teste de utilização do método dentro do método main.
+               */
+
+            Calculadora numero = new Calculadora(2,3);
+            numero.Soma();
+
         }
     }
 }
